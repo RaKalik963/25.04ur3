5a1301a0ac97079632122d7aca29ec10eba4f1d8
RaKalik963 <andrejmahot646@gmail.com>
2024-04-25 20:33:10 +0300

    lab-ex1

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
+	int year;
+	std::cout << "enter year";
+	std::cin >> year;
+	bool is_leap_year = (year % 4 == 0 && year % 100 != 0);
+	int total_days = 365 + static_cast<int>(is_leap_year);
+	std::cout << "in" << year << "year = " << total_days << "days" << std::endl;
 	return 0;
 	system("pause");
 }
