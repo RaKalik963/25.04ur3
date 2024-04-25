5ef10314ca5f1bfcec7e618df1d6a32c07ebea64
RaKalik963 <andrejmahot646@gmail.com>
2024-04-25 20:09:52 +0300

    dz-ex2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-#include <cmath>
+#include <cmath> 
 using namespace std;
-
 int main()
 {
-	return 0;
-	system("pause");
+    double amount;
+    std::cout << "vvedit drobove chislo ";
+    std::cin >> amount;
+    int hrn = static_cast<int>(amount);
+    int kop = round((amount - hrn) * 100);
+    std::cout << " grn " << hrn << " kop" << kop << std::endl;
+    system("pause");
+    return 0;
 }
