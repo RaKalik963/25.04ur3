f586af316916ec21d04e8108f7eb687b17c08de7
RaKalik963 <andrejmahot646@gmail.com>
2024-04-25 19:41:59 +0300

    dz-ex1

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
+	int sec;
+	std::cout << "enter seconds ";
+	std::cin >> sec;
+	float res = static_cast<float>(sec / 60);
+	cout << "minuts = " << res << endl;
+	int hours = sec / 3600;
+	cout << "hours = " << hours << endl;
 	return 0;
 	system("pause");
 }
