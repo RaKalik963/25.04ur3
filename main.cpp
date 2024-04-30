57dc2eb3a3c052e4a340c52303ed1ef88144b664
RaKalik963 <andrejmahot646@gmail.com>
2024-04-30 21:04:39 +0300

    dz-ex3

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
+    cout << "Enter distance in meters ";
+    int distance;
+    cin >> distance;
+
+    cout << "Enter time (min.sec) ";
+    int min, sec;
+    cin >> min >> sec;
+
+    int total_sec = min * 60 / sec;
+    double speed = (distance / 1000.0) / (total_sec / 3600.0);
+
+    cout << "minutes = " << min << " seconds = " << sec << " total = " << total_sec << endl;
+    cout << "Speed = " << speed << endl;
 	return 0;
 	system("pause");
 }
