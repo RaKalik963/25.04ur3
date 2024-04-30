d0cbd7ea4a142544409c8237152eca42a225b96b
RaKalik963 <andrejmahot646@gmail.com>
2024-04-30 21:14:39 +0300

    dz-ex4

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
+    cout << "enter days";
+    int days;
+    cin >> days;
+    int weeks = round(days / 7);
+    int day = days % 7;
+    cout << " weeks = " << weeks << " day = " << day << endl;
 	return 0;
 	system("pause");
 }
