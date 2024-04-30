dc498c24e729f40e4fba0a4c254fab6f7382af04
RaKalik963 <andrejmahot646@gmail.com>
2024-04-30 22:40:59 +0300

    lab-ex22

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
+	int hrn, kop;
+	cout << "enter hrn";
+	cin >> hrn;
+	cout << "enter kop";
+	cin >> kop;
+
+	hrn += kop / 100;
+	kop %= 100;
+	cout << "amount = " << hrn << "." << kop << endl;
 	return 0;
 	system("pause");
 }
