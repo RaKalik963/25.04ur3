cb12cf046287aa1b308f32abe396198f1d05c34d
RaKalik963 <andrejmahot646@gmail.com>
2024-04-30 21:49:56 +0300

    lab-ex3

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 int main()
 {
+	cout << "lenght";
+	double a;
+	cin >> a;
+	cout << "width";
+	double b;
+	cin >> b;
+	cout << "height";
+	double c;
+	cin >> c;
+
+	double V = a * b * c;
+	cout << "V = " << V << endl;
 	return 0;
 	system("pause");
 }
