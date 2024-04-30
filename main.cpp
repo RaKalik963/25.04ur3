a83d7207308063595cc40a2cc042d5ed96f26f38
RaKalik963 <andrejmahot646@gmail.com>
2024-04-30 21:39:06 +0300

    lab-ex4

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
+	cout << "Map scale (number of kilometres in one centimetre)";
+	int scale;
+	cin >> scale;
+
+	cout << "Distance between points representing points (cm)";
+	int distance;
+	cin >> distance;
+
+	double dis_bet_point = scale / 100 * distance;
+	cout << "Distance between points" << dis_bet_point << endl;
 	return 0;
 	system("pause");
 }
