d1fb831d67b0fc5b9b66f36a9bec92c5bd747b09
RaKalik963 <andrejmahot646@gmail.com>
2024-04-30 21:27:48 +0300

    lab-ex5

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
+	cout << "enter radius";
+	int R;
+	cin >> R;
+	double V = (4 / 3) * 3.14 * (R * R * R);
+	cout << "V = " << V << endl;
 	return 0;
 	system("pause");
 }
